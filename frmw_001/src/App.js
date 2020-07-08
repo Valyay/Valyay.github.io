c500e2fad04e8bca35c04136aae6c860f3567232
Valyay <evgesha.valyaev@gmail.com>
2020-07-08 21:15:26 +0300

    Done frmw_001

--- frmw_001/src/App.js
+++ frmw_001/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [value, setValue] = useState("");
+	return (
+		<div style={{ display: "flex", flexDirection: "column", width: "300px" }}>
+			<input
+				value={value}
+				onChange={event => setValue(event.target.value)}
+			></input>
+			<input
+				value={value}
+				onChange={event => setValue(event.target.value)}
+			></input>
+		</div>
+	);
 }
 
 export default App;
